0b98725f32dd551573bf29af38978640aae1b052
ArrojoAleeaMae <aleeamaea@gmail.com>
2022-03-09 19:25:48 +0800

    My Educational Background added

--- git-python-Arrojo.py
+++ git-python-Arrojo.py
@@ -8,3 +8,6 @@ Created on Wed Mar  9 19:07:26 2022
 print("Aleea Mae Meneses Arrojo")
 print("Cawit Boac Marinduque")
 print("May 08, 2002")
+print("Marinduque State College")
+print("Bachelor of Science in Information Technology")
+print("2-C")
