8044cd52f082012bd532dda7d9bf8a87ff45e0b6
ArrojoAleeaMae <aleeamaea@gmail.com>
2022-03-09 19:12:04 +0800

    My Personal Infor added

---
+++ git-python-Arrojo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 19:07:26 2022
+
+@author: ALEEA MAE ARROJO
+"""
+
+print("Aleea Mae Meneses Arrojo")
+print("Cawit Boac Marinduque")
+print("May 08, 2002")
